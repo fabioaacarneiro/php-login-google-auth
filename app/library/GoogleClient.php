4ed472f8f506ef1cc54411aeadbdfe86a39b89ee
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-02-20 00:37:15 -0300

    finished get data login from google auth

--- app/library/GoogleClient.php
+++ app/library/GoogleClient.php
@@ -3,11 +3,14 @@
 namespace app\library;
 
 use Google\Client;
+use Google\Service\Oauth2;
+use Google\Service\Oauth2\Userinfo;
 use GuzzleHttp\Client as GuzzleClient;
 
 class GoogleClient
 {
     public readonly Client $client;
+    private Userinfo $data;
     public function __construct()
     {
         $this->client = new Client;
@@ -21,7 +24,7 @@ class GoogleClient
             ]
         ]);
         $this->client->setHttpClient($guzzleClient);
-        $this->client->setAuthConfig("credentials.json");
+        $this->client->setAuthConfig(__DIR__ . "/../../credentials.json");
         $this->client->setRedirectUri("http://localhost:9090");
         $this->client->addScope("email");
         $this->client->addScope("profile");
@@ -36,7 +39,16 @@ class GoogleClient
     {
         if (isset($_GET["code"])) {
             $token = $this->client->fetchAccessTokenWithAuthCode($_GET["code"]);
-            var_dump($token);
+            $this->client->setAccessToken($token["access_token"]);
+
+            $googleService = new Oauth2($this->client);
+            $this->data = $googleService->userinfo->get();
+            // var_dump($this->data);
         }
+    }
+
+    public function getData()
+    {
+        return $this->data;
     }
 }
